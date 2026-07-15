9d016432f7f6b83a8aee7d3b4dd4a55df129cbd3
moyuyangyangyang <1320644739@qq.com>
2026-07-15 14:53:50 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+window.classFundStore = {
+    version: 1,
+    totalBudget: 3200,
+    generatedAt: "2026-07-15T06:50:13.330Z",
+    recordCount: 24,
+    chunkFiles: [
+        "classFundData.chunk.001.js",
+        "classFundData.chunk.002.js",
+        "classFundData.chunk.003.js",
+    ]
+};
+window.classFundDataChunks = [];
+window.registerClassFundChunk = function registerClassFundChunk(chunk) {
+    if (!window.classFundDataChunks) {
+        window.classFundDataChunks = [];
+    }
+    window.classFundDataChunks.push(chunk);
+};
